1ceba99c7a893b672157e9b059765eedfd500a31
grand <lucas.grando@vortigo.com.br>
2021-10-04 10:29:36 -0300

    Create base_url build config

--- app/src/main/java/com/grando/pokemonapp/framework/api/PokemonApiClient.kt
+++ app/src/main/java/com/grando/pokemonapp/framework/api/PokemonApiClient.kt
@@ -4,13 +4,14 @@ import com.apollographql.apollo.ApolloClient
 import com.apollographql.apollo.ApolloQueryCall
 import com.apollographql.apollo.api.Response
 import com.apollographql.apollo.coroutines.await
+import com.grando.pokemonapp.BuildConfig
 import com.grando.pokemonapp.GetPokemonQuery
 import com.grando.pokemonapp.GetPokemonsQuery
 
 object PokemonApiClient {
 
     private fun apolloClient(): ApolloClient =
-        ApolloClient.builder().serverUrl("https://graphql-pokemon2.vercel.app").build()
+        ApolloClient.builder().serverUrl(BuildConfig.BASE_URL).build()
 
     fun getPokemons(first: Int): ApolloQueryCall<GetPokemonsQuery.Data> =
         apolloClient().query(GetPokemonsQuery(first))
